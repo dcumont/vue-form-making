53e5a351343e1cd1b03919ed4804c17604d18aed
Jean de Quatrebarbes <jdequatrebarbes@zenvalue.fr>
2020-07-06 12:48:26 +0200

    Fix: remove empty form-item in form-maker

--- src/components/WidgetConfig.vue
+++ src/components/WidgetConfig.vue
@@ -304,7 +304,7 @@
       </template>
       
 
-      <template v-if="data.type != 'grid'">
+      <template v-if="data.type != 'grid' && data.type != 'imgupload'">
         <el-form-item :label="$t('fm.config.widget.attribute')">
           <el-checkbox v-model="data.options.readonly" v-if="Object.keys(data.options).indexOf('readonly')>=0">{{$t('fm.config.widget.readonly')}}</el-checkbox>
           <el-checkbox v-model="data.options.disabled" v-if="Object.keys(data.options).indexOf('disabled')>=0">{{$t('fm.config.widget.disabled')}}	</el-checkbox>
